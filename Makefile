9183ae603fd0df9f0addf5fca5cfc410d72e6c2e
Mirko Mariotti <mirko@mirkomariotti.it>
2025-03-17 08:56:59 +0100

    :wrench: (Makefile) bm project files

---
+++ Makefile
@@ -0,0 +1,1 @@
+../conproc/vivado_tools/Makefile
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-numterations=1
+import os
+numiterations=1
 numsamples=10
 checksimulation=True
 checkbmhardware=True
 checkhlshardware=True
+os.environ["PATH"]=os.environ["PATH"]+":/home/mirko/.go/bin"
