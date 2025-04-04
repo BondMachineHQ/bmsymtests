d1b68da8324e9453153713988688a0cb44450d09
Mirko Mariotti <mirko@mirkomariotti.it>
2025-04-04 14:28:02 +0200

    :wrench: (analyze) modular design

--- config.py
+++ config.py
@@ -1,9 +1,2 @@
 import os
-numiterations=1
-numsamples=10
-checksimulation=True
-checkbmhardware=True
-checkhlshardware=True
-prefix="0f"
 os.environ["PATH"]=os.environ["PATH"]+":/home/mirko/.go/bin"
-dontGenerate=False
--- local.mk
+++ local.mk
@@ -1,17 +1,18 @@
 ROOTDIR=.
 WORKING_DIR=working_dir
 CURRENT_DIR=$(shell pwd)
-SOURCE_FLEXPY=generated.py
 FLEXPY_APP=working_dir/expression.c
 FLEXPY_ARGS=--basm --build-app --app-file $(FLEXPY_APP) --emit-bmapi-maps --bmapi-maps-file bmapi.json --io-mode sync --config-file flexpyconfig.json --neuron-statistics statistics.json
 FLEXPY_LIB=library
 BOARD=zedboard
 BASM_ARGS=-disable-dynamical-matching -chooser-min-word-size -chooser-force-same-name
+BASM_LIB_FILES=library/*basm
 MAPFILE=zedboard_maps.json
 SHOWARGS=-dot-detail 5
 SHOWRENDERER=dot -Tpng > bondmachine.png
-EXTRACLEAN=*.csv *.seq num_outputs.txt bmapi.json bondmachine.png statistics.json zedboard*
+EXTRACLEAN=*.csv *.seq num_outputs.txt bmapi.json bondmachine.png statistics.json zedboard* source.mk out.basm
 include bmapi.mk
 include deploy.mk
 include simbatch.mk
 include simulation.mk
+include source.mk
