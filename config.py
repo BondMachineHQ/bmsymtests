e941e05b77dc78f00afc52b38b6b10e036090c66
Mirko Mariotti <mirko@mirkomariotti.it>
2025-10-31 11:00:41 +0100

    :sparkles: (analyze) debug flag

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 os.environ["PATH"]=os.environ["PATH"]+":/tools/Xilinx/Vivado/2023.2/bin"
 os.environ["PATH"]=os.environ["PATH"]+":/home/mirko/.go/bin"
 
+debug=False
 singleRun=False
 singleRunPatch=False
 fullRun=False
